5a40e78a4c40980a7bf10913ce1f0486286c1b65
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-02-09 06:09:12 -0800

    going into footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-gray-300">
+      <div className="max-w-7xl mx-auto px-6 py-16 grid grid-cols-2 md:grid-cols-4 gap-12">
+        
+        
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
