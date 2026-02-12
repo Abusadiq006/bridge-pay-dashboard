26e01eb5e30c4ef91a5029f08c142148c169865e
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-02-12 01:58:26 -0800

    implementing CommAndPayouts

---
+++ src/pages/CommAndPayouts.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import { ArrowDownLeft, ArrowUpRight, DollarSign, Wallet } from 'lucide-react'
+import Sidebar from '../components/Sidebar'
+import Header from '../components/Header'
+
+const payoutHistory = [
+]
