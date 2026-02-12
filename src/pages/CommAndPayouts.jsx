ba78b992dc543c6168ded5a9b2181d5ee5d0fd67
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-02-12 02:06:51 -0800

    implementing functions

--- src/pages/CommAndPayouts.jsx
+++ src/pages/CommAndPayouts.jsx
@@ -8,3 +8,9 @@ const payoutHistory = [
     { id: 'TRX002', date: 'Feb 05, 2026', vendor: 'Jane Smith', amount: 3500, status: 'Processing'},
     { id: 'TRX003', date: 'Feb 01, 2026', vendor: 'John Doe', amount: 1200, status: 'Failed' },
 ]
+
+const CommAndPayouts = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+    
+}
