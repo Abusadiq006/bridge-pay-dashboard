f5c5732cac57a443bacb15b0916e4f26d148b21e
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-02-13 06:18:18 -0800

    implement Market page

---
+++ src/pages/Market.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import { Search, Star, MapPin } from 'lucide-react'
+import Sidebar from '../components/Sidebar'
+import Header from '../components/Header'
+
+
+
+export default Market
