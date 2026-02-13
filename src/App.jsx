5c8e3234d08ce2767b95527b0c9670442e932437
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-02-13 06:22:48 -0800

    implement Market page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import DashboardPage from "./pages/DashboardPage";
 import CommAndPayouts from './pages/CommAndPayouts';
 import AddRequest from "./pages/AddRequest";
+import Market from "./pages/Market";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/dashboard/payouts" element={<CommAndPayouts />} />
         <Route path="/dashboard/add-request" element={<AddRequest />} />
+        <Route path="/dashboard/market" element={<Market />} />
       </Routes>
     </Router>
   )
