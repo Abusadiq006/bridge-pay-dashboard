681cd9a09e01c9f396ab7f27a868fcbea64df43c
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-02-26 07:49:05 -0800

    implementing import for notifications

---
+++ src/pages/Notifications.jsx
@@ -0,0 +1,4 @@
+import { useState } from 'react';
+import { Bell, CheckCircle, Clock, AlertCircle, Trash2 } from 'lucide-react';
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
