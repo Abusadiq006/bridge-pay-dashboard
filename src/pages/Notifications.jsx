5ef15582edcde369966fb2a232e5a04e2233df53
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-02-26 07:49:49 -0800

    implementing export for notifications

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -2,3 +2,8 @@ import { useState } from 'react';
 import { Bell, CheckCircle, Clock, AlertCircle, Trash2 } from 'lucide-react';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
+
+
+
+
+export default Notifications;
