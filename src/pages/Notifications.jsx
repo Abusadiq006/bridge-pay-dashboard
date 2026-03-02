8687f43e9851b8a74e4ca1849ef98391ac510628
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-02 02:51:42 -0800

    initialnotification 2

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 
 const initialNotifications = [
   { id: 1, type: 'payment', title: 'Payment Recieved', message: 'You received $500 from Sarah Connor for "Wedding Cake".', time: '2 mins ago', read: false },
+  { id: 2, type: 'payout', title: 'Payout Successful', message: 'Your payout of $1,200 has been processed to your bank.', time: '1 hour ago', read: true },
 ]
 
 export default Notifications
