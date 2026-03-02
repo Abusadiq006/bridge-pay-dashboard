27c8a2e9d0799758a55e4205d89b146fece21b07
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-02 02:51:11 -0800

    initialnotification

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -2,5 +2,8 @@ import { useState} from 'react'
 import Sidebar from '../components/Sidebar'
 import Header from '../components/Header'
 
+const initialNotifications = [
+  { id: 1, type: 'payment', title: 'Payment Recieved', message: 'You received $500 from Sarah Connor for "Wedding Cake".', time: '2 mins ago', read: false },
+]
 
 export default Notifications
