a4ec34e320ff4b04190312238549954c627e166b
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-02 03:21:03 -0800

    implementing main

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -22,6 +22,8 @@ const Notifications = () => {
 
         <div className="flex-1 flex flex-col">
           <Header onMenuClick={() => setIsSidebarOpen(true)} />
+
+          <main></main>
         </div>
     </div>
   )
