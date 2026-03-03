e81becc2023c5434ff01da0947f4ead06230c65c
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-03 05:10:25 -0800

    implementing const

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { Eye, EyeOff, Lock, Mail } from 'lucide-react'
 
-
+const Login = () => {
+    const [showPassword, setShowPassword] = useState(false)
+    const [formData, setFormData] = useState({ email:'', password:''})
+    const navigate = useNavigate()
+}
 
 export default Login
