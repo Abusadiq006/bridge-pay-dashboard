9b40c432311d696bc487d73714eb298643784c8e
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-03 05:16:10 -0800

    simulating login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,10 @@ const Login = () => {
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
-        
+        e.preventDefault()
+        localStorage.setItem('isLoggedIn', 'true')
+        localStorage.setItem('userToken', 'dummy-token-123')
+        navigate('/dashboard')
     }
 }
 
