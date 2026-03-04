1cff1d87eb491f9de57d753c72494127d2c06d53
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-04 07:04:13 -0800

    implementing Signup const

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,5 +2,9 @@ import React, { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { Eye, EyeOff, Lock, Mail, User, Building2, CheckCircle } from 'lucide-react'
 
+const Signup = () => {
+    const [showPassword, setShowPassword] = useState(false)
+    
+}
 
 export default Signup
