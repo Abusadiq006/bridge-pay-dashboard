820082120a0f7071e242c2036af2cb6a075f6050
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-04 06:56:50 -0800

    Initiating rxport

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { Eye, EyeOff, Lock, Mail, User, Building2, CheckCircle } from 'lucide-react'
+
+
+export default Signup
