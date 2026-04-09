9a2125077bb5e64917ce90bae52791e171b228fa
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-09 04:46:27 -0700

    const response

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,5 +3,7 @@ import axios from 'axios';
 const API_URL = 'http://localhost:5000/api/auth';
 
 export const loginUser = async (email, password) => {
-    try {}
+    try {
+        const response = await axios.post(`${API_URL}/login`, { email, password }); 
+    }
 }
